907bc20867ec1c36a4d9fb53849337e99e78670b
Josh Trommel <joshtrommel@gmail.com>
2016-11-12 16:19:43 -0800

    A bit of a cleanup

--- index.js
+++ index.js
@@ -17,8 +17,7 @@ var mongoose   = require('mongoose');
 mongoose.connect('mongodb://node:node@novus.modulusmongo.net:27017/Iganiq8o'); // connect to our database
 var Bear     = require('./app/models/bear');
 
-// ROUTES FOR OUR API
-// =============================================================================
+// api routes
 
 // create our router
 var router = express.Router();
@@ -37,45 +36,7 @@ router.get('/', function(req, res) {
 	res.json({ message: 'hooray! welcome to our api!' });
 });
 
-router.get('/', function(req, res) {
-	res.json({ message: 'hooray! welcome to our api!' });
-});
 
-
-
-
-// on routes that end in /bears
-// ----------------------------------------------------
-router.route('/bears')
-
-	// create a bear (accessed at POST http://localhost:8080/bears)
-	.post(function(req, res) {
-
-		var bear = new Bear();		// create a new instance of the Bear model
-		bear.name = req.body.name;  // set the bears name (comes from the request)
-
-		bear.save(function(err) {
-			if (err)
-				res.send(err);
-
-			res.json({ message: 'Bear created!' });
-		});
-
-
-	})
-
-	// get all the bears (accessed at GET http://localhost:8080/api/bears)
-	.get(function(req, res) {
-		Bear.find(function(err, bears) {
-			if (err)
-				res.send(err);
-
-			res.json(bears);
-		});
-	});
-
-// on routes that end in /bears/:bear_id
-// ----------------------------------------------------
 router.route('/bears/:bear_id')
 
 	// get the bear with that id
@@ -117,11 +78,10 @@ router.route('/bears/:bear_id')
 		});
 	});
 
-
-// REGISTER OUR ROUTES -------------------------------
+// register routes
 app.use('/api', router);
 
-// START THE SERVER
-// =============================================================================
-app.listen(port);
-console.log('Magic happens on port ' + port);
+// start theory
+app.listen(port, function() {
+	console.log('Magic happens on localhost:' + port);
+});
