c6d5c702ee11ddfc5c67e3c9691fef476fb36928
Ethan Arterberry <ethanarterberry@gmail.com>
2016-11-13 01:42:36 -0800

    FUCK

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require('express')
 	, morgan = require('morgan')
 	, amazon = require('amazon-product-api')
 	, cheerio = require('cheerio')
-	, request = require('request');
+	, request = require('sync-request');
 
 // set up amazon client
 var client = amazon.createClient({
@@ -41,69 +41,54 @@ router.get('/', function(req, res) {
 	res.json({ message: 'api root, baby' });
 });
 
-router.route('/:gender/:style/:article') // article=[top/bottom]
-	.get(function(req, res) {
-		var gender = (function () {
-			if (req.params.gender === "other") {
-				return ""
-			} else {
-				return req.params.gender
-			}
-		})();
-
-		if(!styles.hasOwnProperty(req.params.style)) {
-			res.redirect('/404')
+router.route('/:gender/:style/').get(function(req, res) {
+	var gender = (function () {
+		if (req.params.gender === "other") {
+			return ""
+		} else {
+			return req.params.gender
 		}
+	})();
 
-		if(req.params.article != "top" && req.params.article != "bottom") {
-			res.redirect('/404')
-		}
-
-		var title = req.params.style
-			, min = 0
-			, max = 3
-			, index = Math.floor(Math.random() * (max - min)) + min
-
-		keyword = gender + " " + styles[title][req.params.article][index]
-		
-		client.itemSearch({
-			Keywords: keyword,
-			responseGroup: 'ItemAttributes',
-			searchIndex: 'Fashion'
-		}, function(err, results, response) {
-			if (err) {
-				res.send(err);
-			} else {
-				const base = rand.paul(results);
-				request.get(base["DetailPageURL"][0], function (err, res2, body) {
-					if (err) {
-						res.send(err);
-					}
-					var page = cheerio.load(body);
-					global.item = {
-						"url": base["DetailPageURL"][0],
-						"brand": base["ItemAttributes"][0]["Brand"][0],
-						"color": base["ItemAttributes"][0]["Color"][0],
-						"department": base["ItemAttributes"][0]["Department"][0],
-						"note": base["ItemAttributes"][0]["Feature"], // amazon's writeup of the product
-						"group": base["ItemAttributes"][0]["ProductGroup"][0],
-						"title": base["ItemAttributes"][0]["Title"][0]
-					}
-					global.item["image"] = page('img#landingImage, img.a-dynamic-image').attr("src");
-					global.item["cost"] = new Object();
-					try {
-						global.item["cost"].pretty = base["ItemAttributes"][0]["ListPrice"][0]["FormattedPrice"][0];
-						global.item["cost"].raw = Number.parseInt(base["ItemAttributes"][0]["ListPrice"][0]["Amount"][0], 10);
-					} catch (e) {
-						global.item["cost"].pretty = page('#priceblock_ourprice, #priceblock_saleprice').text();
-						global.item["cost"].raw = Number.parseInt(global.item["cost"].pretty.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, ""), 10);
-					}
-
-					res.send(global.item);
-				});
+	var priceRange = (function () {
+		if (req.query["p"] != undefined) {
+			var max = Number.parseInt(req.query['p']);
+			return {
+				"top": " under $" + max * .4,
+				"bottom": " under $" + max * .6
 			}
-		});
-	});
+		} else {
+			return ""
+		}
+	})();
+
+	if(!styles.hasOwnProperty(req.params.style)) {
+		res.redirect('/404')
+	}
+
+	var title = req.params.style
+		, min = 0
+		, max = 3
+		, index = Math.floor(Math.random() * (max - min)) + min
+
+	global.item = new Object();
+	global.done = new Object();
+	getItem("top", gender, priceRange, title, index);
+	getItem("bottom", gender, priceRange, title, index);
+
+	// very, very, very dirty hack. fix later
+	var checkIfDone = setInterval(function () {
+		if (global.done["top"] && global.done["bottom"]) {
+			clearInterval(checkIfDone);
+			var punctuation = /[$.]/g
+			global.item["totalCost"] = new Object();
+			global.item["totalCost"]["raw"] = global.item["top"]["cost"]["raw"] + global.item["bottom"]["cost"]["raw"];
+			// fix this later
+			// global.item["totalCost"]["pretty"] = "$" + String(parseInt(global.item["top"]["cost"]["pretty"].split(punctuation)[1], 10) + parseInt(global.item["bottom"]["cost"]["pretty"].split(punctuation)[1], 10)) + "." + String(parseInt(global.item["top"]["cost"]["pretty"].split(punctuation)[2], 10) + parseInt(global.item["top"]["cost"]["pretty"].split(punctuation)[2], 10));
+			res.send(global.item);
+		}
+	}, 50);
+});
 
 // register routes
 app.use('/api', router);
@@ -116,3 +101,43 @@ app.get('*', function(req, res){
 app.listen(port, function() {
 	console.log('Magic happens on localhost:' + port);
 });
+
+function getItem(article, gender, priceRange, title, index) {
+	keyword = gender + " " + styles[title][article][index] + priceRange[article];
+	
+	client.itemSearch({
+		Keywords: keyword,
+		responseGroup: 'ItemAttributes',
+		searchIndex: 'Fashion'
+	}).then(function(results) {
+		var base = rand.paul(results);
+		var scrapeRes = request("GET", base["DetailPageURL"][0])
+		var body = scrapeRes.getBody()
+		
+		var page = cheerio.load(body);
+		global.item[article] = {
+			"url": base["DetailPageURL"][0],
+			"brand": base["ItemAttributes"][0]["Brand"][0],
+			"color": base["ItemAttributes"][0]["Color"][0],
+			"department": base["ItemAttributes"][0]["Department"][0],
+			"note": base["ItemAttributes"][0]["Feature"], // amazon's writeup of the product
+			"group": base["ItemAttributes"][0]["ProductGroup"][0],
+			"title": base["ItemAttributes"][0]["Title"][0]
+		}
+
+		global.item[article]["image"] = page('img#landingImage, img.a-dynamic-image').attr("src");
+		global.item[article]["cost"] = new Object();
+
+		try {
+			global.item[article]["cost"].pretty = base["ItemAttributes"][0]["ListPrice"][0]["FormattedPrice"][0];
+			global.item[article]["cost"].raw = Number.parseInt(base["ItemAttributes"][0]["ListPrice"][0]["Amount"][0], 10);
+		} catch (e) {
+			global.item[article]["cost"].pretty = page('#priceblock_ourprice, #priceblock_saleprice').text();
+			global.item[article]["cost"].raw = Number.parseInt(global.item[article]["cost"].pretty.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, ""), 10);
+		}
+
+		global.done[article] = true
+	}).catch(function(err) {
+		res.send(err);
+	});
+}
